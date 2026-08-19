baaf2b0593aedd7f73cdbec9ae288a74aec5e719
igor-lourenco <igor.lourencosantos@gmail.com>
2026-08-19 11:09:34 -0300

    Criação do projeto

---
+++ src/test/java/com/testes/api/rest/assured/RestAssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testes.api.rest.assured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAssuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
